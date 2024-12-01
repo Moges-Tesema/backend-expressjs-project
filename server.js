132abf2ac26904367f0e04b7fb6b0f237f4dcd77
Moges-Tesema <moges2421@gmail.com>
2024-12-01 10:52:58 +0300

    the first scaffolding tool of backend project using expressJs

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+app.get("/",(req,res)=>{
+   
+    res.sendFile(path.join(__dirname,"public","index.html"));
+    
+});
+app.get("/about",(req,res)=>{
+    res.sendFile(path.join(__dirname,"public","about.html"));
+})
+app.get("/contact",(req,res)=>{
+    res.send("<h1>contact page that is amazing right</h1>");
+})
+app.listen(8000,()=>console.log("server is running on port 8000")); 
